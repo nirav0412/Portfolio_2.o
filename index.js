881cc057560c546fddf1177a12634ca30dcacd6f
Nirav Katharotiya <kathrotiyanirav10@gmail.com>
2023-02-28 07:41:57 +0530

    third js commit

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// let docTitle = HomePage;
-// window.addEventListener("blur", () {
-//     document.title = "Comeback :("
-// })
-// window.addEventListener("focus", () {
-//     document.title = "Homepage :("
-// })
 window.onload = function() {
 
     var pageTitle = document.title;
@@ -20,3 +13,43 @@ window.onload = function() {
       }
     });
   };
+
+  // -----------------Pop-up Screen------------------------
+
+  const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
